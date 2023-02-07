21e41ee6a01ea1da8bdc680e01da5066bce55f18
Mayu Imanaka <imamayu.1215@gmail.com>
2023-02-07 21:50:54 +0900

    add blog.tsx page.tsx

---
+++ src/gatsby-note.ts
@@ -0,0 +1,44 @@
+import { GatsbyNode } from 'gatsby'
+import path from 'path'
+
+export const createPage: GatsbyNode['createPages'] = async ({ graphql, actions: { createPage } }) => {
+  const result = await graphql<Queries.CreatePagesQuery>(`
+  query CreatePages { 
+    allMicrocmsBlogs(sort: { order: ASC, fields: publishedAt }) {
+      edges {
+        node {
+          blogsId
+        }
+        next {
+          blogsId
+          title
+        }
+        previous {
+          blogsId
+          title
+        }
+      }
+    }
+  }
+  `)
+
+  if (result.errors){
+    throw result.errors
+  }
+
+  const { allMicrocmsBlogs } = result.data!
+
+  allMicrocmsBlogs.edges.forEach((edge) => {
+    createPage({
+      path: `/blog/${edge.node.blogsId}/`,
+
+      component: path.resolve('src/templates/blog.tsx'),
+
+      context: {
+        id: edge.node.blogsId,
+        next:edge.next,
+        previous: edge.previous
+      }
+    })
+  })
+}
